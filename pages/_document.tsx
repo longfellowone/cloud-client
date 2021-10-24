8c55cf6bb16bb9ad7b3b3b273dc3cc9e7f31cd37
Matt Wright <mattwright3195@gmail.com>
2021-10-24 12:30:53 -0700

    change font to swap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com/"
+            crossOrigin="true"
+          /> */}
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=option"
+            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
           />
         </Head>
